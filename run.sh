788b2ac2973e39d6e2dc00e90e18f6e5c3d12340
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2018-02-17 15:55:06 +1000

    Adding some TODOs

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# TODO: Add check to see if Ansible is installed
+#       If not, install and configure
+
 if [ ! -e "/etc/ansible/hosts" ]; then
     echo "No hosts found in '/etc/ansible/hosts', exiting..."
     exit 1
 fi
 
+# TODO: Scan environment and configure accordingly
+
+# TODO: For now, do Arch setups only
 ansible-playbook -v arch.yml
