dc667b32196e19691b6fd3a9114ad9c650e53cd7
Pedro Barbosa <pedbarbosa@gmail.com>
2021-07-22 10:50:46 +1000

    Adding Ubuntu support (#6)
    * Updated path for Arch files
    
    * Added configuration for Debian hosts
    
    * Added postfix to logrotate

--- run.sh
+++ run.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
 
-# TODO: Add check to see if Ansible is installed
-#       If not, install and configure
+function is_debian {
+    [[ -e /etc/debian_version ]]
+}
 
-if [ ! -e "/etc/ansible/hosts" ]; then
-    echo "No hosts found in '/etc/ansible/hosts', exiting..."
-    exit 1
+# Check if Ansible is installed
+if ! command -v ansible &> /dev/null; then
+    echo "Ansible isn't installed!"
+    if is_debian; then
+        echo "Calling 'apt' to install Ansible ..."
+        sudo apt install -y ansible sshpass
+    fi
 fi
 
-# TODO: Scan environment and configure accordingly
+# Check if localhost is defined as a host
+if [ ! -f "/etc/ansible/hosts" ]; then
+  echo touch /etc/ansible/hosts
+fi
 
-# TODO: For now, do Arch setups only
-ansible-playbook -v arch.yml
+if ! grep '127.0.0.1' /etc/ansible/hosts &> /dev/null; then
+    echo "Updating Ansible hosts file ..."
+    echo '127.0.0.1' | sudo tee -a /etc/ansible/hosts
+fi
+
+if is_debian; then
+  playbook=ubuntu
+else
+  playbook=arch
+fi
+
+ansible-playbook -CD "$playbook".yml
