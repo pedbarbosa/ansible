b0b0c0ae63a652a17a97039897845885f2a345d9
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2026-09-07 21:20:08 +1000

    Updated run script

--- run.sh
+++ run.sh
@@ -13,9 +13,38 @@ if ! command -v ansible &> /dev/null; then
     fi
 fi
 
+public_key_file="$HOME/.ssh/id_ed25519.pub"
+authorized_keys_file="$HOME/.ssh/authorized_keys"
+current_user=$(whoami)
+
+if [[ ! -f "$public_key_file" ]]; then
+    echo "SSH ID file '$public_key_file' doesn't exist, creating ..."
+    ssh-keygen -t ed25519 -f "$public_key_file" -C '$(hostname)' -N ""
+fi
+
+if [[ ! -f "$authorized_keys_file" ]]; then
+  echo "Creating '$authorized_keys_file' file ..."
+  touch "$authorized_keys_file"
+  chmod 600 "$authorized_keys_file"
+fi
+
+public_key=$(cat "$public_key_file")
+if ! grep -qF "$public_key" "$authorized_keys_file"; then
+    echo "'$authorized_keys_file' requires an update ..."
+    echo "$public_key" >> "$authorized_keys_file"
+else
+    echo "'$authorized_keys_file' is up-to-date."
+fi
+
 # Check if localhost is defined as a host
 if [ ! -f "/etc/ansible/hosts" ]; then
-  echo touch /etc/ansible/hosts
+    sudo mkdir -p /etc/ansible
+    sudo touch /etc/ansible/hosts
+fi
+
+if [[ ! -f "/etc/sudoers.d/$current_user" ]]; then
+    echo "Adding user '$current_user' to sudoers ..."
+    echo "$current_user ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/$current_user
 fi
 
 if ! grep '127.0.0.1' /etc/ansible/hosts &> /dev/null; then
